e33d54c814f5cb2a037bdd09f2bd9cc9fc8ea911
shadowl143 <42244456+shadowl143@users.noreply.github.com>
2022-04-28 20:05:08 -0600

    bucles

---
+++ Bucles/Bucles/Mientras.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucles
+{
+    public  class Mientras
+    {
+
+
+        public Mientras()
+        {
+
+        }
+
+
+        public void EjecutarMientras(int opc)
+        {
+            int contador=0;
+            while (contador < opc)
+            {
+                Console.WriteLine("Entro " + contador);
+                contador++;
+            }
+        }
+
+        public void EjecutarMientras(string opc)
+        {
+            while ("Llover" == opc)
+            {
+                opc = Clima();
+            }
+        }
+
+
+        public void RevisarClima()
+        {
+            var opc = "Llover";
+            var i = 0;
+            do
+            {
+                if (i == 5)
+                {
+                    opc ="Soleado";
+                }
+                i++;
+            } while ("Llover" == opc);
+        }
+        public string Clima()
+        {
+            var contador = 0;
+            while (contador<10)
+            {
+                if (contador == 5)
+                {
+                    return "Soleado";
+                }
+                contador++;
+            }
+            return "";
+        }
+    }
+}
---
+++ Bucles/Bucles/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Bucles;
+/**
+* while 
+* do while
+* for
+* foreach
+* metodos recursivos
+* */
+
+/**
+ * usando el while necesitamos contar del 1 hasta al 20 en forma progresiva mostrar cada uno de los numeros
+ * usando el do while vamos a realizar un contador del 20 al cero en forma regresiva mostrar cada uno de los numeros
+ * **/
+Mientras mientras = new Mientras();
+mientras.RevisarClima();
+
+
