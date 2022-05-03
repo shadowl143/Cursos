b8206088a38b91480ae9de5dfe8118d25325c4d9
shadowl143 <42244456+shadowl143@users.noreply.github.com>
2022-05-02 20:02:42 -0600

    Uso del For

---
+++ Bucles/Bucles/Para.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucles
+{
+    public class Para
+    {
+        int numero=10;
+
+        public Para()
+        {
+            int numero = 2;
+            Console.WriteLine(numero);
+            Console.WriteLine(this.numero);
+
+        }
+
+
+        public void EjecutarFor()
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            //int resultado = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                //resultado=resultado+i;
+                Console.WriteLine(i);
+            }
+            timer.Stop();
+            Console.WriteLine("El tiempo de ejecucion fue " + timer.ElapsedMilliseconds);
+        }
+
+        public void EjecutarForMultiplicar()
+        {
+            int tabla = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+
+                Console.WriteLine(tabla * i);
+            }
+        }
+
+        public void EjecutarForDosEnDos()
+        {
+            // int tabla = Convert.ToInt32(Console.ReadLine());
+            int interaccion = 1;
+            for (int i = 0; i <= 50; i += 2)
+            {
+                //i=i+3 === i+=3 i=0 i=2 i=4 i=6
+                Console.Write(i + ",");
+                interaccion++;
+            }
+            Console.WriteLine("numero de iteraciones: " + interaccion);
+        }
+
+        int contador = 0;
+        public void EjecutarRecursividad(int i)
+        {
+
+            if (i < 10)
+            {
+                contador++;
+                Console.WriteLine(this.contador);
+                EjecutarRecursividad(this.contador);
+            }
+        }
+
+        public void TomarTiempo()
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            EjecutarRecursividad(0);
+            timer.Stop();
+            Console.WriteLine("El tiempo de ejecucion fue " + timer.ElapsedMilliseconds);
+
+        }
+    }
+}
--- Bucles/Bucles/Program.cs
+++ Bucles/Bucles/Program.cs
@@ -5,15 +5,42 @@ using Bucles;
 * while 
 * do while
 * for
-* foreach
+* foreach, List array
 * metodos recursivos
 * */
 
+//*Se necersitan arreglos no realizar
+// * Un equipo de basquetboll necesitamos saber quien anota mas puntos utilizando un for acomoda estos jugador 
+// * en orden de mayor a menos la cantidad de puntos anotados no puedes utilizar el orden acomodado tu, utiliza el for para acomodarlos.
+#region Mientras
 /**
  * usando el while necesitamos contar del 1 hasta al 20 en forma progresiva mostrar cada uno de los numeros
  * usando el do while vamos a realizar un contador del 20 al cero en forma regresiva mostrar cada uno de los numeros
  * **/
-Mientras mientras = new Mientras();
-mientras.RevisarClima();
+//Mientras mientras = new Mientras();
+//mientras.RevisarClima();
+#endregion
+
+#region Para
+/**
+ * 
+
+ * 
+ * realiza un contador de tres en tres empezando del 0
+ * 
+ * recorrer MOD 1%2=0 un cantidad de 50 numeros mostrando el numero x es par o el numero x es impar utilizando metodo recursivo y for
+ * 
+ * pide al usuario la tabla que desea conocer y muestrala en pantalla 1 *1 =1 1*2=2
+ * 
+ * 
+ * */
+Para para = new Para();
+//para.EjecutarFor();
+//para.EjecutarForMultiplicar();
+//para.EjecutarForDosEnDos();
+//para.TomarTiempo(); 
+#endregion
+
+
 
 
